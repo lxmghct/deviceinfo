531dd7ef08745d5af75c5d50f21fb41f4e590bb4
longxiaoming <1781092881@qq.com>
2025-12-24 02:26:24 +0800

    add xposed

---
+++ app/src/main/java/com/example/deviceinfo/MainHook.java
@@ -0,0 +1,13 @@
+package com.example.deviceinfo;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XposedBridge;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    }
+}
